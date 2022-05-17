ba3d3b4479e35563529f67f6e8accc359005c387
rkirk430 <r.kirk430@gmail.com>
2022-05-17 13:00:50 -0400

    Invalid API Token was Supplied Error

--- src/pages/price.js
+++ src/pages/price.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 const Price = (props) => {
-    return <h1>this is the Price History Page </h1> 
+    //Start with our API Key
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const params = useParams();
+    console.log(params);
+    const ticker = params.ticker;
+    console.log(ticker);
+
+    const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${apiKey}`;
+
+    const [price, setPrice] = useState("null");
+
+    const getPrice = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setPrice(data);
+    };
+
+    useEffect(() => {
+        getPrice();
+    }, []);
+
+    const loaded = () => {
+        return (
+            <div>
+                <h1>Stock {price.data[0].ticker} </h1> 
+            </div>
+        );
+    }; 
 };
 
 export default Price;
