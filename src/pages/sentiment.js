482c114672122c9007eaa723c8a2707570137d83
rkirk430 <r.kirk430@gmail.com>
2022-05-17 16:01:22 -0400

    Sentiment Data JSON Pulling Through

--- src/pages/sentiment.js
+++ src/pages/sentiment.js
@@ -1,7 +1,21 @@
 import React from "react";
+import sentimentData from "../sentiment.json";
+console.log(sentimentData); // Renders Object Array from "sentiment.json"
 
-const Sentiment = (props) => {
-    return <h1>This is the Sentiment Page</h1>
-};
+const Sentiment = () => {
+    const allSentimentData = sentimentData.results.map((sentiment,idx) => {
+        // console.log(sentiment); // Gives me all of the obejcts within the Array
+        return (
+            <p key = {idx}> {sentiment.rank}. {sentiment.name} {sentiment.ticker} {sentiment.upvotes} </p>
+        )
+    })
+        return(
+            <div className="sentiment">
+                {allSentimentData}
+            </div>
+        )
+}
+
+
 
 export default Sentiment;
