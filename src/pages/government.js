c35dae0e270bd4a19b171ce22eba12e895ca3f15
rkirk430 <r.kirk430@gmail.com>
2022-05-17 18:39:50 -0400

    Adding Government Data

--- src/pages/government.js
+++ src/pages/government.js
@@ -1,7 +1,23 @@
 import React from "react";
+import governmentData from "../government.json";
+console.log(governmentData); // Renders as Object with Arrays in console
 
-const Government = (props) => {
-    return <h1> This is the Gov't Insiders Trading Page </h1>
-};
+const Government = () => {
+    const allGovernmentData = governmentData[0].map((insiderTrades, idx) => {
+        return (
+            <p key = {idx}> {insiderTrades.representative}</p>
+        )
+    })
+        return(
+            <div className="government">
+                {allGovernmentData}
+            </div>
+        )
+}
+
+
+// const Government = (props) => {
+//     return <h1> This is the Gov't Insiders Trading Page </h1>
+// };
 
 export default Government;
