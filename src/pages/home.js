b21c0795452bea19daefcf9ad568f8d16160def4
rkirk430 <r.kirk430@gmail.com>
2022-05-17 13:45:31 -0400

    Rendering static data in JSON: Console Log

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import data from "../data.json"
-console.log(data);
+
 
 const Home = (props) => {
     return <h1>This is the Homepage</h1> 
--- src/pages/price.js
+++ src/pages/price.js
@@ -1,37 +1,75 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router";
+import data from "../data.json"
+console.log(data);
 
 const Price = (props) => {
-    //Start with our API Key
-    const apiKey = process.env.REACT_APP_API_KEY;
-    const params = useParams();
-    console.log(params);
-    const ticker = params.ticker;
-    console.log(ticker);
-
-    const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${apiKey}`;
-
-    const [price, setPrice] = useState("null");
-
-    const getPrice = async () => {
-        const response = await fetch(url);
-        const data = await response.json();
-        console.log(data);
-        setPrice(data);
-    };
-
-    useEffect(() => {
-        getPrice();
-    }, []);
-
-    const loaded = () => {
-        return (
-            <div>
-                <h1>Stock {price.data[0].ticker} </h1> 
-            </div>
-        );
-    }; 
+    return(
+        <h1> This is the Price History Page </h1>
+     );
 };
 
 export default Price;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ====================================================================
+//                      Using API
+// ====================================================================
+
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router";
+
+// const Price = (props) => {
+//     //Start with our API Key
+//     const apiKey = process.env.REACT_APP_API_KEY;
+//     const params = useParams();
+//     console.log(params);
+//     const ticker = params.ticker;
+//     console.log(ticker);
+
+//     const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${apiKey}`;
+
+//     const [price, setPrice] = useState("null");
+
+//     const getPrice = async () => {
+//         const response = await fetch(url);
+//         const data = await response.json();
+//         console.log(data);
+//         setPrice(data);
+//     };
+
+//     useEffect(() => {
+//         getPrice();
+//     }, []);
+
+//     const loaded = () => {
+//         return (
+//             <div>
+//                 <h1>Stock {price.data[0].ticker} </h1> 
+//             </div>
+//         );
+//     }; 
+// };
+
+// export default Price;
