4437c7149828a244b92a57198cfb6f5f4c9cc00b
rkirk430 <r.kirk430@gmail.com>
2022-05-18 11:29:25 -0400

    Struggling with Pricing

--- src/pages/price.js
+++ src/pages/price.js
@@ -1,18 +1,66 @@
 import React from "react";
-import stockData from "../data.json"
-console.log(stockData);
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+// import stockData from "../data.json"
+// console.log(stockData);
 
+// ====================================================================
+//                      Using API (Second Pass)
+// ====================================================================
 
+// const Price = () => {
+//     //State to hold Ticker Symbol
+//     const [tickers, setTickers] = useState([]);
 
+//     //State to hold Pricing
+// }
 
+// ====================================================================
+//                      Using API (First Pass)
+// ====================================================================
+    
+    //ISSUES: 
+        //Not pulling any data so console.log(params) is not returning anything. 
 
+const Price = () => {
+    //Start with our API Key
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const params = useParams();
+    console.log(params); //** DISPLAYS AN EMPTY OBJECT */
+    const ticker = params.price.data.params;
+    console.log(ticker);
 
+    const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${apiKey}`;
 
+    const [price, setPrice] = useState("null");
 
+    const getPrice = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setPrice(data);
+    };
 
+    useEffect(() => {
+        getPrice();
+    }, []);
 
+    const loaded = () => {
+        return (
+            <div>
+                <h1>Stock {price.data[0].ticker} </h1> 
+            </div>
+        );
+    }; 
 
+    const loading = () => {
+        return <h1> Loading... </h1>
+    };
 
+    return price ? loaded() : loading();
+};
+
+export default Price;
 
 
 
@@ -20,87 +68,20 @@ console.log(stockData);
 //========================================================================
 //                  Working Static Price 
 //========================================================================
-const Price = () => {
-    const allStockData = stockData.data.map((price,idx) => {
-        console.log(idx);
-        return (
-            <p key = {idx}> {price.name} {price.ticker} ${price.price} </p>
-        )
-    })
-        return(
-        <div className="pricing">
-            {allStockData}
-        </div>
-    )
-}
-
-
-export default Price;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ====================================================================
-//                      Using API
-// ====================================================================
-
-// import { useState, useEffect } from "react";
-// import { useParams } from "react-router";
-
-// const Price = (props) => {
-//     //Start with our API Key
-//     const apiKey = process.env.REACT_APP_API_KEY;
-//     const params = useParams();
-//     console.log(params);
-//     const ticker = params.ticker;
-//     console.log(ticker);
-
-//     const url = `https://api.stockdata.org/v1/data/quote?symbols=${ticker}&api_token=${apiKey}`;
-
-//     const [price, setPrice] = useState("null");
-
-//     const getPrice = async () => {
-//         const response = await fetch(url);
-//         const data = await response.json();
-//         console.log(data);
-//         setPrice(data);
-//     };
-
-//     useEffect(() => {
-//         getPrice();
-//     }, []);
-
-//     const loaded = () => {
+// const Price = () => {
+//     const allStockData = stockData.data.map((price,idx) => {
+//         console.log(idx);
 //         return (
-//             <div>
-//                 <h1>Stock {price.data[0].ticker} </h1> 
-//             </div>
-//         );
-//     }; 
-// };
+//             <p key = {idx}> {price.name} {price.ticker} ${price.price} </p>
+//         )
+//     })
+//         return(
+//         <div className="pricing">
+//             {allStockData}
+//         </div>
+//     )
+// }
+
 
 // export default Price;
-
-
-
-//Source Data: https://www.stockdata.org/ 
+//Source Data: https://www.stockdata.org/;
