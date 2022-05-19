ddac308e8bf89bd66e41c66cacadb26f1cccaf9d
rkirk430 <r.kirk430@gmail.com>
2022-05-19 00:48:18 -0400

    Homepage Styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 
 const Home = (props) => {
     return(
         <>
-            <h3> Alternative Market Research for the Disruptive Economy </h3>
-            <h5> Quantstamp analyzes millions of datapoints to provide granular, actionable insights to underlying market sentiment </h5>
+            <Container fluid>
+                <Row className="homepage-header">
+                    <Col sm={8}></Col>
+                        <h4> Alternative Market Research for the Disruptive Economy. </h4>
+                    <Col sm={4}></Col>
+                         <h6> Quantstamp analyzes millions of datapoints to provide granular, actionable insights to underlying market sentiment </h6>
+                </Row>
+                <Row className="homepage-benefits-container">
+                    <Col sm={4}></Col>
+                        <h6> API Model Integration </h6>
+                        <smaller> What does it mean? </smaller>
+                    <Col sm={4}></Col>
+                        <h6> Real Time Trade Alerts </h6>
+                        <smaller> What does it mean? </smaller>
+                    <Col sm={4}></Col>
+                        <h6> Improving Risk-Adjusted Returns </h6>
+                        <smaller> What does it mean? </smaller>
+                </Row>
+            </Container>
         </>
 
     ) 
